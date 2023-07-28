baba9b6bd4517fda2b7575a0aa2e2a7ef9d5f2ab
NiclanX <nicardolannaman@gmail.com>
2023-07-28 02:17:05 -0500

    Commit 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import { Navbar } from './Navbar';
 import './App.css';
+import { Notes } from './Notes';
 
 function App() {
   return (
+    <>
    <Navbar/>
+   <div className="container">
+    <Notes/>
+   </div>
+   
+   </>
   );
 }
 
