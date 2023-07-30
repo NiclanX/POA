8102ed3cbd65e43a5578437dfc336a13365f91b5
NiclanX <nicardolannaman@gmail.com>
2023-07-30 05:32:43 -0500

    Commit 9

--- src/Notes.jsx
+++ src/Notes.jsx
@@ -1,17 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Notes = () => {
 
     
-const [notes, setNotes] = useState([
-    {title: 'The Dynamic Bomboclaat', body: 'Lorem fam', id: 1},
-    {title: 'The highest level of Fuckery', body: 'Lorem fam', id: 2},
-    {title: 'BLOUSE AND SKIRT', body: 'Lorem fam', id: 3},
-    {title: 'I should be asleep', body: 'Lorem fam', id: 4},
-    {title: 'Oh fucking really', body: 'Lorem fam', id: 5},
-    {title: 'Jah Jah God lol', body: 'Lorem fam', id: 6}
+const [notes, setNotes] = useState(null);
+
+useEffect(()=> {
+    fetch('http://localhost:8000/notes')
+    .then(res => {
+       return res.json()        
+    })
+    .then( (data)=> {
+        setNotes(data)
+        console.log(data)
+    })
     
-]);
+},[])
 
   return (
     <>
@@ -19,18 +23,18 @@ const [notes, setNotes] = useState([
         <h1 className='notes-head'>All Notes</h1>
         <button>Create Note!</button>
     </div>
-    <div className="notes">
+    {notes && <div className="notes">
         
         {notes.map((note) => {
           return (  
-            <div className="note-preview">
+            <div className="note-preview" key={note.id}>
                 <h2>{note.title}</h2>
                 <br />
                 <p>{note.body}</p>
             </div>)
             
         })}
-    </div>
+    </div>}
 
     
     </>
